2f33d0844d0e1636832a956f024a74a3f87b4043
Sunil Verma <sunilverma00027@gmail.com>
2025-12-17 23:18:51 +0530

    Data Transformation Completed

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import sys
-from text_summarizer.pipeline.stage_02_data_validation import DataValidationTrainPipeline
 from pathlib import Path
+from text_summarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainPipeline
+from text_summarizer.pipeline.stage_02_data_validation import DataValidationTrainPipeline
+from text_summarizer.pipeline.stage_03_data_transformation import DataTransformationTrainPipeline
+from text_summarizer.logging import logger
+
 
 # Ensure local src/ is on the path when running directly
 sys.path.insert(0, str(Path(__file__).resolve().parent / "src"))
-
-from text_summarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainPipeline
-from text_summarizer.logging import logger
 
 STAGE_NAME = "Data Ingestion Stage"
 try: 
@@ -18,11 +19,24 @@ except Exception as e:
     logger.exception(f"Error in stage {STAGE_NAME}")
     raise e
 
+
 STAGE_NAME = "Data Validation Stage"
 try: 
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_validation = DataValidationTrainPipeline()
     data_validation.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e: 
+    logger.exception(f"Error in stage {STAGE_NAME}")
+    raise e
+
+
+STAGE_NAME = "Data Transformation Stage"
+try: 
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    from text_summarizer.pipeline.stage_03_data_transformation import DataTransformationTrainPipeline
+    data_transformation = DataTransformationTrainPipeline()
+    data_transformation.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e: 
     logger.exception(f"Error in stage {STAGE_NAME}")
---
+++ src/text_summarizer/pipeline/stage_03_data_transformation.py
@@ -0,0 +1,12 @@
+from text_summarizer.config.configuration import ConfigurationManager
+from text_summarizer.components.data_transformation import DataTransformation
+from text_summarizer.logging import logger 
+
+class DataTransformationTrainPipeline: 
+    def __init__(self):
+        pass
+    def main(self):
+        config = ConfigurationManager() 
+        data_transformation_config = config.get_data_transformation_config() 
+        data_transformation = DataTransformation(config = data_transformation_config) 
+        data_transformation.convert()
